dcedcd90bad1f25679f7e3ab2d1437b7bdd24cea
SchneePingu <>
2021-04-21 00:21:48 +0200

    refactor system tests

--- tests/py/TestHelper.py
+++ tests/py/TestHelper.py
@@ -4,32 +4,15 @@ import os
 
 class Test_Helper():
     def __init__(self):
-        self.path_to_tests_directory = self.__get_path_to_tests_directory()
-        self.path_to_test_file_system = self.__get_path_to_test_file_system()
-        self.path_to_kitty_executable = self.__get_path_to_kitty_executable()
-
-    def __get_path_to_tests_directory(self):
-        path_to_tests_directory = os.path.join(os.path.dirname(__file__), '..')
-
-        return path_to_tests_directory
-
-    def __get_path_to_test_file_system(self):
-        name_of_test_file_system = 'data'
-        path_to_test_file_system = os.path.join(self.path_to_tests_directory, name_of_test_file_system)
-
-        return path_to_test_file_system
-
-    def __get_path_to_kitty_executable(self):
-        path_to_kitty_executable = os.path.join(self.path_to_tests_directory, '..', 'sh' ,'kitty')
-
-        return path_to_kitty_executable
-
-    def change_to_test_file_system(self):
-        os.chdir(self.path_to_test_file_system)
+        self._path_to_data = os.path.join('tests', 'data')
+        self._path_to_kitty = os.path.join('..', '..', 'sh' ,'kitty')
 
     def run_kitty(self, *args):
         kitty_arguments = list(args)
-        search_result = subprocess.run([self.path_to_kitty_executable] + kitty_arguments, stdout=subprocess.PIPE) \
+
+        search_result = subprocess.run([self._path_to_kitty] + kitty_arguments, \
+            stdout=subprocess.PIPE, \
+            cwd=self._path_to_data) \
             .stdout.decode('utf-8').strip().split(os.linesep)
 
         return search_result
