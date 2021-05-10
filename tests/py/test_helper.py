f47f6701cd3843cda565b8e0323b59a87e1064e6
SchneePingu <>
2021-05-10 20:47:48 +0200

    refactor system tests

--- tests/py/test_helper.py
+++ tests/py/test_helper.py
@@ -1,27 +1,22 @@
 """
-This module provides a class to run the 'kitty' command in test code.
+This module provides a method to run the 'kitty' command in test code.
 """
 
 import subprocess
 import os
-import re
 
 
-class TestHelper:
-    """Class to run the 'kitty' command in test code."""
+def run_kitty(*args):
+    """Run 'kitty' executable with command line arguments."""
+    path_to_data = os.path.join('tests', 'data')
+    path_to_kitty = os.path.join('..', '..', 'sh', 'kitty')
 
-    def __init__(self):
-        self._path_to_data = os.path.join('tests', 'data')
-        self._path_to_kitty = os.path.join('..', '..', 'sh', 'kitty')
+    kitty_arguments = list(args)
 
-    def run_kitty(self, *args):
-        """Run 'kitty' executable with command line arguments."""
-        kitty_arguments = list(args)
+    search_result = subprocess.run([path_to_kitty] + kitty_arguments, \
+        stdout=subprocess.PIPE, \
+        cwd=path_to_data, \
+        check=True) \
+        .stdout.decode('utf-8').strip()
 
-        search_result = subprocess.run([self._path_to_kitty] + kitty_arguments, \
-            stdout=subprocess.PIPE, \
-            cwd=self._path_to_data, \
-            check=True) \
-            .stdout.decode('utf-8').strip()
-
-        return search_result.split(os.linesep)
+    return search_result.split(os.linesep)
